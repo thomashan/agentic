c518c9b1c9bba7a0f1878bea462f44272192daba
Thomas Han <thomashan@users.noreply.github.com>
2025-09-17 13:34:08 +1000

    tools: open webui (#32)

--- install/llm/local/local.mk
+++ install/llm/local/local.mk
@@ -7,9 +7,13 @@ include make/config.mk
 DETECTED_OS := $(shell uname -s)
 
 install-llm-local-ollama:
-	ansible-playbook install/llm/local/ollama-setup.yml;
+	ansible-playbook install/llm/local/ollama-setup.yml
+
+install-llm-local-open-webui:
+	ansible-playbook install/llm/local/open-webui-setup.yml
 
 install-llm-local-help:
 	@echo "install/llm/local/local.mk targets"
 	@printf $(HELP_FORMAT) "install-llm-local-ollama" "Installs Ollama."
+	@printf $(HELP_FORMAT) "install-llm-local-open-webui" "Start Open WebUI."
 	@printf $(HELP_FORMAT) "install-llm-local-help" "Shows this help message."
